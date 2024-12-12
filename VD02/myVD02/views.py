c1d8c34d9fdca99b85cb51f85ff3c65ca2b5b091
demonomed85 <politeh@mail.ru>
2024-12-12 21:49:37 +0300

    Init

---
+++ VD02/myVD02/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def new(request):
+    return render(request, 'main/new.html')
+
+def test(request):
+    return HttpResponse('Это страница test!')
+# Create your views here.
